7fbde8c9fdb054fc8f6ffdfe8299c62f6df09b38
Aarna Katyal <katyalaarna14@gmail.com>
2026-03-04 19:05:13 +0530

    Adding both files for UC6

---
+++ UC6/OOPSBannerApp6.java
@@ -0,0 +1,57 @@
+public class OOPSBannerApp6 {
+
+    public static String[] getOPattern() {
+        return new String[] {
+            "  *****  ",
+            " *     * ",
+            " *     * ",
+            " *     * ",
+            " *     * ",
+            " *     * ",
+            "  *****  "
+        };
+    }
+
+    public static String[] getPPattern() {
+        return new String[] {
+            " ******  ",
+            " *     * ",
+            " *     * ",
+            " ******  ",
+            " *       ",
+            " *       ",
+            " *       "
+        };
+    }
+
+   
+    public static String[] getSPattern() {
+        return new String[] {
+            "  *****  ",
+            " *     * ",
+            " *       ",
+            "  *****  ",
+            "       * ",
+            " *     * ",
+            "  *****  "
+        };
+    }
+
+    public static void main(String[] args) {
+
+        String[] lines = new String[7];
+
+        String[] OPattern = getOPattern();
+        String[] pPattern = getPPattern();
+        String[] sPattern = getSPattern();
+
+        
+        for (int i = 0; i < lines.length; i++) {
+            lines[i] = OPattern[i] + "  " + OPattern[i] + "  " + pPattern[i] + "  " + sPattern[i];
+        }
+
+        for (String line : lines) {
+            System.out.println(line);
+        }
+    }
+}
