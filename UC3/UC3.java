120cb148a9ba5946226a965627efad2f5de17866
Aarna Katyal <katyalaarna14@gmail.com>
2026-04-12 12:57:37 +0530

    Added UC3

---
+++ UC3/UC3.java
@@ -0,0 +1,17 @@
+package UC3;
+
+public class UC3 {
+   public static void main(String[] args) {
+
+        String name;
+
+        if (args.length > 0 && !args[0].isEmpty()) {
+            name = args[0];
+        } else {
+            name = "World";
+        }
+
+        System.out.println("Hello " + name);
+    }
+}
+    
